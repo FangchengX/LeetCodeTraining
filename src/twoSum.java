d81e51ab453c344735d6caede4d7cf4d46c4f5c0
kq6441 <kq6441@163.com>
2017-08-29 15:27:33 +0800

    twoSum

--- src/twoSum.java
+++ src/twoSum.java
@@ -1,10 +1,30 @@
+import java.util.HashMap;
+
 /**
  * Created by equation on 17-8-29.
  */
 public class twoSum {
-    public int[] twoSum(int[] nums, int target) {
-        int[] a = new int[2];
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int[] result = new int[2];
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],i);
+        }
 
-        return a;
+        for(int i=0;i<nums.length;i++){
+            int temp = target - nums[i];
+            if(map.containsKey(temp)&&map.get(temp)!=i){
+                int j = map.get(temp);
+                if(i<j){
+                    result[0] = i;
+                    result[1] = j;
+                }else{
+                    result[0] = j;
+                    result[1] = i;
+                }
+                break;
+            }
+        }
+        return result;
     }
 }
