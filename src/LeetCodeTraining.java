c580a3e7e479105f1b48e93e2ff2cdbaaade3733
kq6441 <kq6441@163.com>
2017-08-29 14:35:35 +0800

    init

---
+++ src/LeetCodeTraining.java
@@ -0,0 +1,8 @@
+/**
+ * Created by equation on 17-8-29.
+ */
+public class LeetCodeTraining {
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/twoSum.java
@@ -0,0 +1,10 @@
+/**
+ * Created by equation on 17-8-29.
+ */
+public class twoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] a = new int[2];
+
+        return a;
+    }
+}
